e337f6fcd44a25b8e1ef4d5d58b8aa6cc67cd0c1
Mako Kusuda <mako.kusuda@gmail.com>
2020-05-16 21:30:05 +0900

    Add clear button and validation

--- public/app.js
+++ public/app.js
@@ -6,16 +6,29 @@ const filterName = document.getElementById("filter-name");
 const filterGrade1 = document.getElementById("filter-grade1");
 const filterGrade2 = document.getElementById("filter-grade2");
 const filterGrade3 = document.getElementById("filter-grade3");
+const filterClear = document.getElementById("filter-clear");
 
 const addButton = document.getElementById("add-button");
 const addName = document.getElementById("add-name");
 const addGrade = document.getElementById("add-grade");
+const addError = document.getElementById("add-error");
+const errorConfirm = document.getElementById("error-confirm");
+const addConfirm = document.getElementById("add-confirm");
+
 const updateDisplayArea = document.getElementById("updated-display-area");
 
 const displayToPage = (value) => {
-  const studentInfo = document.createElement("p");
+  const card = document.createElement("div");
+  const studentInfo = document.createElement("a");
   studentInfo.innerHTML = "Name : " + value.name + ", Grade : " + value.grade;
-  listDisplayArea.appendChild(studentInfo);
+  studentInfo.setAttribute("id", `sid${value.id}`);
+  const editInfo = document.createElement("button");
+  editInfo.innerHTML = "Edit";
+
+  card.appendChild(studentInfo);
+  card.appendChild(editInfo);
+
+  listDisplayArea.appendChild(card);
 };
 
 //search and display all students
@@ -83,10 +96,19 @@ filterButton.addEventListener("click", () => {
     });
 });
 
+filterClear.addEventListener("click", () => {
+  filterId.value = "";
+  filterName.value = "";
+  filterGrade1.checked = false;
+  filterGrade2.checked = false;
+  filterGrade3.checked = false;
+  listDisplayArea.innerHTML = "";
+});
+
 //add student
 addButton.addEventListener("click", () => {
   if (!addName.value || !addGrade.value) {
-    console.log("cannot add!");
+    addError.style.display = "block";
   } else {
     fetch("/graphql", {
       method: "POST",
@@ -104,10 +126,23 @@ addButton.addEventListener("click", () => {
       .then((res) => res.json())
       .then((res) => {
         updateDisplayArea.innerHTML = "";
+        const addedInfo = document.createElement("div");
+        addedInfo.innerHTML = `Name: ${addName.value}, Grade: ${addGrade.value}`;
+        updateDisplayArea.appendChild(addedInfo);
+        addConfirm.style.display = "block";
       })
       .then(() => {
         addName.value = "";
         addGrade.value = "";
       });
   }
+});
+
+addConfirm.addEventListener("click", () => {
+  updateDisplayArea.innerHTML = "";
+  addConfirm.style.display = "none";
+});
+
+errorConfirm.addEventListener("click", () => {
+  addError.style.display = "none";
 });
